1209a959b9a675a2426df6a9c166a4c58d048c7c
tanveer-hash <73640862+tanveer-hash@users.noreply.github.com>
2020-10-29 19:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def get_data():
-    url = "https://www.instagram.com/sidhu_moosewala/?__a=1"
-    return requests.get(url).content
+    return "Hello World"
 app.run()

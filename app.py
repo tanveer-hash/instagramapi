4e402b0b71daeebb2c0ce29dc9147f5825a7fa16
tanveer-hash <73640862+tanveer-hash@users.noreply.github.com>
2020-10-30 17:39:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import flask
 from flask_restful import Api, Resource, reqparse, request
 import requests
 app = flask.Flask(__name__)
-app.run(port=8080)
 @app.route('/', methods=['GET'])
 def get_data():
     return "Hello World"
